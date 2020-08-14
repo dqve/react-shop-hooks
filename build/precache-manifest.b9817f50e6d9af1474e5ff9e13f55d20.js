62acf0349ab0e1bd5981519e381af84f1e6f6d7a
David Ayoola <ayooladavid@gmail.com>
2020-08-14 02:33:52 +0000

    Built project

---
+++ build/precache-manifest.b9817f50e6d9af1474e5ff9e13f55d20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff38c075cf53bb8173bdf4b5d7eaeb7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3783f24726108d42c550",
+    "url": "/static/css/main.2e06b7fe.chunk.css"
+  },
+  {
+    "revision": "6681abbf7465b84e40c9",
+    "url": "/static/js/2.66d6b445.chunk.js"
+  },
+  {
+    "revision": "3783f24726108d42c550",
+    "url": "/static/js/main.77a1dc58.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
